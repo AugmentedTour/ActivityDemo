409603ead13da9daa11b12e76624b132d8794730
Bridget Wellen <bridgetwellen@hotmail.com>
2019-06-11 00:43:04 -0700

    A B C and N buildings

---
+++ SeniorProjectWithBackbone/app/src/main/java/com/example/robin/seniorprojectwithbackbone/ABCFoundation.java
@@ -0,0 +1,40 @@
+package com.example.robin.seniorprojectwithbackbone;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+
+
+public class ABCFoundation extends AppActivityBuilderMethods {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // --- Toolbar stuff, don't forget to set the name ---
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Bellevue College Foundation");
+        setSupportActionBar(toolbar);
+
+        // --- Gotta put these in the onCreate method ---
+        LinearLayout topLayout = (LinearLayout) findViewById(R.id.topLayout);
+        LinearLayout bodyLayout = (LinearLayout) findViewById(R.id.bodyLayout);
+
+
+        // --- topLayout ---
+        subTitleBuilder("Bellevue College Foundation", topLayout);
+
+        String info = "The Bellevue College Foundation is a non-profit, 501(c)3 organization aligned with Bellevue College. \n" +
+                "The Foundation, through a unique blend of cutting-edge, innovative and exemplary programs, will enable " +
+                "Bellevue College to attract, without financial barriers, the best and brightest students and faculty. We " +
+                "strive to provide opportunities for all students to change their lives and become exceptional.";
+
+        // --- bodyLayout ---
+        textViewBuilder(info, bodyLayout);
+        phoneBuilder(" BC Foundation ", "(425) 564-2386", bodyLayout);
+        linkButtonBuilder("BC Foundation Website", "https://www.bellevuecollege.edu/foundation/", true, bodyLayout);
+
+    }
+
+}
